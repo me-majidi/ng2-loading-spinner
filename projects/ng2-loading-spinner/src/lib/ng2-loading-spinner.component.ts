b052fe86ba5454f2e70d51102f4fb99a28614683
Mehran <mehraanmajidi@yahoo.com>
2018-07-29 16:35:29 +0430

    Create ng2-loading-spinner project

---
+++ projects/ng2-loading-spinner/src/lib/ng2-loading-spinner.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'enl-ng2-loading-spinner',
+  template: `
+    <p>
+      ng2-loading-spinner works!
+    </p>
+  `,
+  styles: []
+})
+export class Ng2LoadingSpinnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/ng2-loading-spinner/src/lib/ng2-loading-spinner.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { Ng2LoadingSpinnerComponent } from './ng2-loading-spinner.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [Ng2LoadingSpinnerComponent],
+  exports: [Ng2LoadingSpinnerComponent]
+})
+export class Ng2LoadingSpinnerModule { }
