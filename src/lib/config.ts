2aed295b5721e56051300fed4b5c90e668a47d3a
Mehran <mehraanmajidi@yahoo.com>
2018-08-01 23:18:34 +0430

    Initial commit

---
+++ src/lib/config.ts
@@ -0,0 +1,29 @@
+export interface INg2LoadingSpinnerConfig {
+    animationType?: string,
+    backdropColor?: string,
+    backdropBorderRadius?: string,
+    spinnerColor?: string,
+    spinnerPosition?: string,
+    spinnerSize?: string,
+    spinnerFontSize?: string
+}
+
+export class Ng2LoadingSpinnerConfig implements INg2LoadingSpinnerConfig {
+    animationType;
+    backdropColor: string;
+    spinnerColor: string;
+    spinnerPosition: string;
+    backdropBorderRadius: string;
+    spinnerSize: string;
+    spinnerFontSize: string;
+
+    constructor(config : INg2LoadingSpinnerConfig) {
+        this.animationType = config.animationType;
+        this.backdropColor = config.backdropColor;
+        this.spinnerColor = config.spinnerColor;
+        this.spinnerPosition = config.spinnerPosition;
+        this.backdropBorderRadius = config.backdropBorderRadius;
+        this.spinnerSize = config.spinnerSize;
+        this.spinnerFontSize = config.spinnerFontSize;
+    }
+}
