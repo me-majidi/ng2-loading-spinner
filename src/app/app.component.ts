8ed85dbd47c755f8e38f4d58881d44d37e3b61dc
Mehran <mehraanmajidi@yahoo.com>
2018-08-01 21:37:21 +0430

    add option for using custom template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { ANIMATION_TYPES } from '../../projects/ng2-loading-spinner/src/public_api';
 import { INg2LoadingSpinnerConfig } from '../../projects/ng2-loading-spinner/src/lib/config';
 import { NgForm } from '@angular/forms';
@@ -9,8 +9,10 @@ import { NgForm } from '@angular/forms';
     styleUrls  : [ './app.component.scss' ]
 })
 export class AppComponent {
+    @ViewChild('customTemplate') customTemplate: TemplateRef;
     show = false;
     borderRadius = 15;
+    template = null;
 
     loadingConfig: INg2LoadingSpinnerConfig = {
         animationType  : ANIMATION_TYPES.dualCircle,
@@ -32,5 +34,11 @@ export class AppComponent {
 
     onChangeOptions(form: NgForm) {
         this.loadingConfig.backdropBorderRadius = form.value.backdropBorderRadius + 'px';
+
+        if (form.value.customTemplate) {
+            this.template = this.customTemplate;
+        } else {
+            this.template = null;
+        }
     }
 }
